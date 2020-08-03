b15543680a10d5adb8cc9e25d46e8735da58da92
Martinus Suherman <martinus.suherman@gmail.com>
2020-08-04 00:36:27 +0700

    Add multi role attribute

---
+++ Misc/ApiRole.cs
@@ -0,0 +1,43 @@
+namespace PsefApi.Misc
+{
+    /// <summary>
+    /// Api role.
+    /// </summary>
+    internal class ApiRole
+    {
+        /// <summary>
+        /// Verifikator role.
+        /// </summary>
+        internal const string Verifikator = "Psef.Verifikator";
+
+        /// <summary>
+        /// Kepala seksi role.
+        /// </summary>
+        internal const string Kasi = "Psef.Kasi";
+
+        /// <summary>
+        /// Kepala subdit role.
+        /// </summary>
+        internal const string Kasubdit = "Psef.Kasubdit";
+
+        /// <summary>
+        /// Diryanfar role.
+        /// </summary>
+        internal const string Diryanfar = "Psef.Diryanfar";
+
+        /// <summary>
+        /// Dirjen role.
+        /// </summary>
+        internal const string Dirjen = "Psef.Dirjen";
+
+        /// <summary>
+        /// Admin role.
+        /// </summary>
+        internal const string Admin = "Psef.Admin";
+
+        /// <summary>
+        /// Super Admin role.
+        /// </summary>
+        internal const string SuperAdmin = "SuperAdmin";
+    }
+}
---
+++ Misc/MultiRoleAuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace PsefApi.Misc
+{
+    /// <summary>
+    /// Enum based multi role authorization attribute
+    /// </summary>
+    public class MultiRoleAuthorizeAttribute : AuthorizeAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the MultiRoleAuthorizeAttribute.
+        /// </summary>
+        /// <param name="allowedRoles">List of allowed roles.</param>
+        public MultiRoleAuthorizeAttribute(params string[] allowedRoles)
+        {
+            AllowedRoles = allowedRoles;
+            Roles = string.Join(',', allowedRoles);
+        }
+
+        /// <summary>
+        /// Gets list of roles that are allowed to access the resource.
+        /// </summary>
+        /// <value>Array of the list of roles.</value>
+        public string[] AllowedRoles { get; }
+    }
+}
