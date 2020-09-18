884d742d1f956e98e41864b016d4899c77c16ed2
Martinus Suherman <martinus.suherman@gmail.com>
2020-09-18 16:00:13 +0700

    Oss model (WIP)

---
+++ Models/ViewModels/OssFullInfo.cs
@@ -0,0 +1,407 @@
+using System;
+using System.Collections.Generic;
+
+namespace PsefApiOData.Models
+{
+    /// <summary>
+    /// Represents a Full OSS Information.
+    /// </summary>
+    public class OssFullInfo
+    {
+        /// <summary>
+        /// Gets or sets the Keterangan.
+        /// </summary>
+        /// <value>The Keterangan.</value>
+        public string Keterangan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Nib.
+        /// </summary>
+        /// <value>The Nib.</value>
+        public string Nib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglPengajuanNib.
+        /// </summary>
+        /// <value>The TglPengajuanNib.</value>
+        public DateTime TglPengajuanNib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglTerbitNib.
+        /// </summary>
+        /// <value>The TglTerbitNib.</value>
+        public DateTime TglTerbitNib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglPerubahanNib.
+        /// </summary>
+        /// <value>The TglPerubahanNib.</value>
+        public DateTime TglPerubahanNib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OssId.
+        /// </summary>
+        /// <value>The OssId.</value>
+        public string OssId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JenisPelakuUsaha.
+        /// </summary>
+        /// <value>The JenisPelakuUsaha.</value>
+        public string JenisPelakuUsaha { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoNpp.
+        /// </summary>
+        /// <value>The NoNpp.</value>
+        public string NoNpp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoVa.
+        /// </summary>
+        /// <value>The NoVa.</value>
+        public string NoVa { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoWlkp.
+        /// </summary>
+        /// <value>The NoWlkp.</value>
+        public string NoWlkp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FlagPerusahaan.
+        /// </summary>
+        /// <value>The FlagPerusahaan.</value>
+        public string FlagPerusahaan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FlagEkspor.
+        /// </summary>
+        /// <value>The FlagEkspor.</value>
+        public string FlagEkspor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FlagImpor.
+        /// </summary>
+        /// <value>The FlagImpor.</value>
+        public string FlagImpor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JenisApi.
+        /// </summary>
+        /// <value>The JenisApi.</value>
+        public string JenisApi { get; set; }
+
+        /// <summary>
+        /// Gets or sets the GabungNegara.
+        /// </summary>
+        /// <value>The GabungNegara.</value>
+        public string GabungNegara { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NegaraPmaDominan.
+        /// </summary>
+        /// <value>The NegaraPmaDominan.</value>
+        public string NegaraPmaDominan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TotalPma.
+        /// </summary>
+        /// <value>The TotalPma.</value>
+        public decimal TotalPma { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NilaiPmaDominan.
+        /// </summary>
+        /// <value>The NilaiPmaDominan.</value>
+        public decimal NilaiPmaDominan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NilaiPmdn.
+        /// </summary>
+        /// <value>The NilaiPmdn.</value>
+        public decimal NilaiPmdn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PersenPma.
+        /// </summary>
+        /// <value>The PersenPma.</value>
+        public short PersenPma { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PersenPmdn.
+        /// </summary>
+        /// <value>The PersenPmdn.</value>
+        public short PersenPmdn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the KdKawasan.
+        /// </summary>
+        /// <value>The KdKawasan.</value>
+        public int KdKawasan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JenisKawasan.
+        /// </summary>
+        /// <value>The JenisKawasan.</value>
+        public string JenisKawasan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the VersiPia.
+        /// </summary>
+        /// <value>The VersiPia.</value>
+        public string VersiPia { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JangkaWaktu.
+        /// </summary>
+        /// <value>The JangkaWaktu.</value>
+        public DateTime? JangkaWaktu { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StatusBadanHukum.
+        /// </summary>
+        /// <value>The StatusBadanHukum.</value>
+        public string StatusBadanHukum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StatusPenanamanModal.
+        /// </summary>
+        /// <value>The StatusPenanamanModal.</value>
+        public string StatusPenanamanModal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NpwpPerseroan.
+        /// </summary>
+        /// <value>The NpwpPerseroan.</value>
+        public string NpwpPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NamaPerseroan.
+        /// </summary>
+        /// <value>The NamaPerseroan.</value>
+        public string NamaPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NamaSingkatan.
+        /// </summary>
+        /// <value>The NamaSingkatan.</value>
+        public string NamaSingkatan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JenisPerseroan.
+        /// </summary>
+        /// <value>The JenisPerseroan.</value>
+        public string JenisPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StatusPerseroan.
+        /// </summary>
+        /// <value>The StatusPerseroan.</value>
+        public string StatusPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AlamatPerseroan.
+        /// </summary>
+        /// <value>The AlamatPerseroan.</value>
+        public string AlamatPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RtRwPerseroan.
+        /// </summary>
+        /// <value>The RtRwPerseroan.</value>
+        public string RtRwPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the KelurahanPerseroan.
+        /// </summary>
+        /// <value>The KelurahanPerseroan.</value>
+        public string KelurahanPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PerseroanDaerahId.
+        /// </summary>
+        /// <value>The PerseroanDaerahId.</value>
+        public string PerseroanDaerahId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the KodePosPerseroan.
+        /// </summary>
+        /// <value>The KodePosPerseroan.</value>
+        public string KodePosPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NomorTelponPerseroan.
+        /// </summary>
+        /// <value>The NomorTelponPerseroan.</value>
+        public string NomorTelponPerseroan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the EmailPerusahaan.
+        /// </summary>
+        /// <value>The EmailPerusahaan.</value>
+        public string EmailPerusahaan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DalamBentukUang.
+        /// </summary>
+        /// <value>The DalamBentukUang.</value>
+        public decimal DalamBentukUang { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DalamBentukLain.
+        /// </summary>
+        /// <value>The DalamBentukLain.</value>
+        public string DalamBentukLain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TotalModalDasar.
+        /// </summary>
+        /// <value>The TotalModalDasar.</value>
+        public decimal TotalModalDasar { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TotalModalDitempatkan.
+        /// </summary>
+        /// <value>The TotalModalDitempatkan.</value>
+        public decimal TotalModalDitempatkan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of PemegangSaham.
+        /// </summary>
+        /// <value>The list of PemegangSaham.</value>
+        public List<OssPemegangSaham> PemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoPengesahan.
+        /// </summary>
+        /// <value>The NoPengesahan.</value>
+        public string NoPengesahan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglPengesahan.
+        /// </summary>
+        /// <value>The TglPengesahan.</value>
+        public DateTime TglPengesahan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoAktaLama.
+        /// </summary>
+        /// <value>The NoAktaLama.</value>
+        public string NoAktaLama { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglAktaLama.
+        /// </summary>
+        /// <value>The TglAktaLama.</value>
+        public DateTime TglAktaLama { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoPengesahanLama.
+        /// </summary>
+        /// <value>The NoPengesahanLama.</value>
+        public string NoPengesahanLama { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglPengesahanLama.
+        /// </summary>
+        /// <value>The TglPengesahanLama.</value>
+        public DateTime TglPengesahanLama { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JenisIdUserProses.
+        /// </summary>
+        /// <value>The JenisIdUserProses.</value>
+        public string JenisIdUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoIdUserProses.
+        /// </summary>
+        /// <value>The NoIdUserProses.</value>
+        public string NoIdUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NamaUserProses.
+        /// </summary>
+        /// <value>The NamaUserProses.</value>
+        public string NamaUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the EmailUserProses.
+        /// </summary>
+        /// <value>The EmailUserProses.</value>
+        public string EmailUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the HpUserProses.
+        /// </summary>
+        /// <value>The HpUserProses.</value>
+        public string HpUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AlamatUserProses.
+        /// </summary>
+        /// <value>The AlamatUserProses.</value>
+        public string AlamatUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JnsKelaminUserProses.
+        /// </summary>
+        /// <value>The JnsKelaminUserProses.</value>
+        public string JnsKelaminUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TempatLahirUserProses.
+        /// </summary>
+        /// <value>The TempatLahirUserProses.</value>
+        public string TempatLahirUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TglLahirUserProses.
+        /// </summary>
+        /// <value>The TglLahirUserProses.</value>
+        public DateTime TglLahirUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DaerahIdUserProses.
+        /// </summary>
+        /// <value>The DaerahIdUserProses.</value>
+        public string DaerahIdUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the RtRwUserProses.
+        /// </summary>
+        /// <value>The RtRwUserProses.</value>
+        public string RtRwUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AgamaUserProses.
+        /// </summary>
+        /// <value>The AgamaUserProses.</value>
+        public string AgamaUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StatusPerkawinanUserProses.
+        /// </summary>
+        /// <value>The StatusPerkawinanUserProses.</value>
+        public string StatusPerkawinanUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PekerjaanUserProses.
+        /// </summary>
+        /// <value>The PekerjaanUserProses.</value>
+        public string PekerjaanUserProses { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StatusNib.
+        /// </summary>
+        /// <value>The StatusNib.</value>
+        public string StatusNib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TipeDokumen.
+        /// </summary>
+        /// <value>The TipeDokumen.</value>
+        public string TipeDokumen { get; set; }
+    }
+}
---
+++ Models/ViewModels/OssPemegangSaham.cs
@@ -0,0 +1,104 @@
+namespace PsefApiOData.Models
+{
+    /// <summary>
+    /// Represents a OSS Pemegang Saham Information.
+    /// </summary>
+    public class OssPemegangSaham
+    {
+        /// <summary>
+        /// Gets or sets the JenisPemegangSaham.
+        /// </summary>
+        /// <value>The JenisPemegangSaham.</value>
+        public string JenisPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FlagAsing.
+        /// </summary>
+        /// <value>The FlagAsing.</value>
+        public string FlagAsing { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TotalModalPemegang.
+        /// </summary>
+        /// <value>The TotalModalPemegang.</value>
+        public decimal TotalModalPemegang { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JabatanPemegangSaham.
+        /// </summary>
+        /// <value>The JabatanPemegangSaham.</value>
+        public string JabatanPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NamaPemegangSaham.
+        /// </summary>
+        /// <value>The NamaPemegangSaham.</value>
+        public string NamaPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the JnsIdentitasPemegangSaham.
+        /// </summary>
+        /// <value>The JnsIdentitasPemegangSaham.</value>
+        public string JnsIdentitasPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NoIdentitasPemegangSaham.
+        /// </summary>
+        /// <value>The NoIdentitasPemegangSaham.</value>
+        public string NoIdentitasPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ValidIdentitasPemegangSaham.
+        /// </summary>
+        /// <value>The ValidIdentitasPemegangSaham.</value>
+        public string ValidIdentitasPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NegaraAsalPemegangSaham.
+        /// </summary>
+        /// <value>The NegaraAsalPemegangSaham.</value>
+        public string NegaraAsalPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the PengendaliPemegangSaham.
+        /// </summary>
+        /// <value>The PengendaliPemegangSaham.</value>
+        public string PengendaliPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the NpwpPemegangSaham.
+        /// </summary>
+        /// <value>The NpwpPemegangSaham.</value>
+        public string NpwpPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the AlamatPemegangSaham.
+        /// </summary>
+        /// <value>The AlamatPemegangSaham.</value>
+        public string AlamatPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FaxPemegangSaham.
+        /// </summary>
+        /// <value>The FaxPemegangSaham.</value>
+        public string FaxPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the EmailPemegangSaham.
+        /// </summary>
+        /// <value>The EmailPemegangSaham.</value>
+        public string EmailPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the FlagPajakPemegangSaham.
+        /// </summary>
+        /// <value>The FlagPajakPemegangSaham.</value>
+        public string FlagPajakPemegangSaham { get; set; }
+
+        /// <summary>
+        /// Gets or sets the KetPajakPemegangSaham.
+        /// </summary>
+        /// <value>The KetPajakPemegangSaham.</value>
+        public string KetPajakPemegangSaham { get; set; }
+    }
+}
