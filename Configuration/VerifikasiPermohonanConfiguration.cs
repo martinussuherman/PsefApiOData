3714146bfed839b3345090a1c4158980e3aefe75
Martinus Suherman <martinus.suherman@gmail.com>
2021-10-02 19:05:48 +0700

    Create verifikasi permohonan configuration

---
+++ Configuration/VerifikasiPermohonanConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Mvc;
+using PsefApiOData.Models;
+
+namespace PsefApiOData.Configuration
+{
+    /// <summary>
+    /// Represents the model configuration for Verifikasi Permohonan.
+    /// </summary>
+    public class VerifikasiPermohonanConfiguration : IModelConfiguration
+    {
+        /// <summary>
+        /// Applies model configurations using the provided builder for the specified API version.
+        /// </summary>
+        /// <param name="builder">The <see cref="ODataModelBuilder">builder</see> used to apply configurations.</param>
+        /// <param name="apiVersion">The <see cref="ApiVersion">API version</see> associated with the <paramref name="builder"/>.</param>
+        public void Apply(ODataModelBuilder builder, ApiVersion apiVersion)
+        {
+            EntityTypeConfiguration<VerifikasiPermohonan> verifikasi = builder
+                .EntitySet<VerifikasiPermohonan>(nameof(VerifikasiPermohonan))
+                .EntityType;
+
+            verifikasi.HasKey(p => p.Id);
+            verifikasi
+                .Expand(SelectExpandType.Disabled)
+                .Filter()
+                .OrderBy()
+                .Page(50, 50)
+                .Select();
+        }
+    }
+}
