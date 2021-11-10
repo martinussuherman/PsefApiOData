6ad373d7e3802393846b0adf579f5245720504c9
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-10 17:21:58 +0700

    Add perizinan update model

---
+++ Models/UpdateModels/PerizinanUpdate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PsefApiOData.Models
+{
+    /// <summary>
+    /// Represents a Perizinan Update information.
+    /// </summary>
+    public partial class PerizinanUpdate
+    {
+        /// <summary>
+        /// Gets or sets the associated Permohonan identifier.
+        /// </summary>
+        /// <value>The associated Permohonan identifier.</value>
+        public uint? PermohonanId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the associated Previous Perizinan identifier.
+        /// </summary>
+        /// <value>The associated Previous Perizinan identifier.</value>
+        public uint? PreviousId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Perizinan number.
+        /// </summary>
+        /// <value>The Perizinan's number.</value>
+        public string PerizinanNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Perizinan issued date.
+        /// </summary>
+        /// <value>The Perizinan's issued date.</value>
+        public DateTime IssuedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Perizinan expired date.
+        /// </summary>
+        /// <value>The Perizinan's expired date.</value>
+        public DateTime ExpiredAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Perizinan Tanda Daftar document url.
+        /// </summary>
+        /// <value>The Perizinan's Tanda Daftar document url.</value>
+        public string TandaDaftarUrl { get; set; }
+    }
+}
