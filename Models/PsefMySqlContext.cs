275b3c47f68d804b2e60e624fabc63804172e2fa
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-12 10:33:54 +0700

    Update db context
    - Add IdIzin

--- Models/PsefMySqlContext.cs
+++ Models/PsefMySqlContext.cs
@@ -656,6 +656,13 @@ namespace PsefApiOData.Models
                     .HasCharSet("utf8")
                     .HasCollation("utf8_general_ci");
 
+                entity.Property(e => e.IdIzin)
+                    .IsRequired()
+                    .HasColumnType("tinytext")
+                    .HasDefaultValueSql("''")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
                 entity.Property(e => e.ImbUrl)
                     .IsRequired()
                     .HasColumnType("text")
