a3cad4c502ea95d6f0c1f7a8beaf3fe6d3e9e3ef
Martinus Suherman <martinus.suherman@gmail.com>
2021-11-19 10:11:33 +0700

    Add pemohon update model

---
+++ Models/UpdateModels/PemohonUpdate.cs
@@ -0,0 +1,44 @@
+﻿namespace PsefApiOData.Models
+{
+    /// <summary>
+    /// Represents a Pemohon.
+    /// </summary>
+    public class PemohonUpdate
+    {
+        /// <summary>
+        /// Gets or sets the associated user identifier.
+        /// </summary>
+        /// <value>The associated user identifier.</value>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Pemohon phone number.
+        /// </summary>
+        /// <value>The Pemohon's phone number.</value>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Pemohon address.
+        /// </summary>
+        /// <value>The Pemohon's address.</value>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Pemohon NIB.
+        /// </summary>
+        /// <value>The Pemohon's NIB.</value>
+        public string Nib { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Pemohon company name.
+        /// </summary>
+        /// <value>The Pemohon's company name.</value>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Pemohon penanggung jawab.
+        /// </summary>
+        /// <value>The Pemohon's penanggung jawab.</value>
+        public string PenanggungJawab { get; set; }
+    }
+}
