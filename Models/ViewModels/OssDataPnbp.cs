28a71645c3edc945968e49e6c9f3ef34dd722199
Martinus Suherman <martinus.suherman@gmail.com>
2021-12-16 17:37:13 +0700

    Create OSS Data PNBP view model

---
+++ Models/ViewModels/OssDataPnbp.cs
@@ -0,0 +1,26 @@
+namespace PsefApiOData.Models
+{
+    /// <summary>
+    /// Represents a OSS Data PNBP Information.
+    /// </summary>
+    public class OssDataPnbp
+    {
+        /// <summary>
+        /// Gets or sets the kode akun.
+        /// </summary>
+        /// <value>The kode akun.</value>
+        public string KdAkun { get; set; }
+
+        /// <summary>
+        /// Gets or sets the kode penerimaan.
+        /// </summary>
+        /// <value>The kode penerimaan.</value>
+        public string KdPenerimaan { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nominal.
+        /// </summary>
+        /// <value>The nominal.</value>
+        public string Nominal { get; set; }
+    }
+}
